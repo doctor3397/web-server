9a7aa006eee73a0941fa6e50f66aca7d3b14c8f7
doctor3397 <hs2546@columbia.edu>
2017-01-24 16:10:46 -0500

    Add response header

--- server.rb
+++ server.rb
@@ -15,17 +15,42 @@ loop do                                             # Server runs forever
   end
   puts lines                                        # Output the full request to stdout
 
-  # Match your response with the requested file
+
+  # 3. Match your response with the requested file
   filename = lines[0].gsub(/GET \//, '').gsub(/\ HTTP.*/, '')
   if File.exists?(filename)
     response_body = File.read(filename)
+
+
+    # 4. Add response headers
+    #Successful response header. Note the "200 OK"
+    success_header = []
+    success_header << "HTTP/1.1 200 OK"
+    success_header << "Content-Type: text/html" # should reflect the appropriate content type (HTML, CSS, text, etc)
+    success_header << "Content-Length: #{response_body.length}" # should be the actual size of the response body
+    success_header << "Connection: close"
+    success_header = success_header.join("\r\n")
+
+    response = [success_header, response_body].join("\r\n\r\n")
   else
     response_body = "File Not Found\n" # need to indicate end of the string with \n
+
+    # "Not Found" response header.
+    not_found_header = []
+    not_found_header << "HTTP/1.1 404 Not Found"
+    not_found_header << "Content-Type: text/plain" # is always text/plain
+    not_found_header << "Content-Length: #{response_body.length}" # should the actual size of the response body
+    not_found_header << "Connection: close"
+    not_found_header = not_found_header.join("\r\n")
+
+    response = [not_found_header, response_body].join("\r\n\r\n")
   end
 
-  client.puts(response_body)
+  #client.puts(response_body)
+  client.puts(response)
 
-  #  Create an HTML response
+
+  # 1. Create an HTML response
   # response = "
   #   <!DOCTYPE html>
   #   <html>
@@ -38,7 +63,7 @@ loop do                                             # Server runs forever
   #     </body>
   # </html>"
 
-  # Move your HTML response into a file
+  # 2. Move your HTML response into a file
   # filename = "index.html"
   # response = File.read(filename)
 
